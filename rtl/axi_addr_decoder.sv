55bac868775f0a768b0ada813a960dd9cb1e61dc
nghoangnam25072016-del <nghoangnam25072016@gmail.com>
2026-04-29 18:43:20 -0700

    Update axi_addr_decoder.sv
    Add AXI address decoder

--- rtl/axi_addr_decoder.sv
+++ rtl/axi_addr_decoder.sv
@@ -1,1 +1,17 @@
+module axi_addr_decoder (
+    input  logic [31:0] addr,
+    output logic        sel_s0,
+    output logic        sel_s1
+);
 
+    always_comb begin
+        sel_s0 = 1'b0;
+        sel_s1 = 1'b0;
+
+        if (addr[31:16] == 16'h0000)
+            sel_s0 = 1'b1;
+        else if (addr[31:16] == 16'h0001)
+            sel_s1 = 1'b1;
+    end
+
+endmodule
